56a7f2b39fbf161541e6469c7942c29b1bc32d51
Bamzhie <sheuahmad@gmail.com>
2023-04-01 10:43:54 +0100

    first node commit

---
+++ modules.js
@@ -0,0 +1,9 @@
+const names = require("./names");
+const sayHi = require("./utils");
+const data = require("./alternative-flavor");
+
+require("./sum");
+
+sayHi("susan");
+sayHi(names.peter);
+sayHi(names.john);
---
+++ names.js
@@ -0,0 +1,5 @@
+const secret = "SUOER SECRET";
+const john = "john";
+const peter = "peter";
+
+module.exports = { john, peter };
